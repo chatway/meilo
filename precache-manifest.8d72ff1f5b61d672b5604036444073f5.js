5a707b7bf4a3e4b081e9b64f229acaa89c66dd00
denzlbro <yunusbaev.di@yandex.ru>
2020-02-14 08:30:03 +0600

    Updates

---
+++ precache-manifest.8d72ff1f5b61d672b5604036444073f5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84c62d7de2d466fdc27a2b1a8ae4cfca",
+    "url": "/chatway/meilo/index.html"
+  },
+  {
+    "revision": "0879e31c1c979505fdbb",
+    "url": "/chatway/meilo/static/css/2.ae2f4c93.chunk.css"
+  },
+  {
+    "revision": "d99f5f33a339a8d947bc",
+    "url": "/chatway/meilo/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "0879e31c1c979505fdbb",
+    "url": "/chatway/meilo/static/js/2.aa0d4056.chunk.js"
+  },
+  {
+    "revision": "d99f5f33a339a8d947bc",
+    "url": "/chatway/meilo/static/js/main.b2201f45.chunk.js"
+  },
+  {
+    "revision": "6de710ee13e983ca08c7",
+    "url": "/chatway/meilo/static/js/runtime-main.d13c4817.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/chatway/meilo/static/media/persik.4e1ec840.png"
+  }
+]);
